1b14bbcdd0a4575a8959f89b2bf2ef26e3f087ff
polyhistor <pouya.ataei.7@gmail.com>
2020-12-04 15:07:27 +1300

    parsed URL and written a conditional proxy

--- serviceWorker.js
+++ serviceWorker.js
@@ -8,12 +8,27 @@ try {
 
 self.addEventListener('fetch' , event => {
   console.log(`fetch event fired ${event.request.url}`)
+  const parsedUrl = new URL(event.request.url);
+  console.log(event)
+  console.log(parsedUrl)
+
+  if (parsedUrl.pathname === "/") {
+    return;
+  }
+
   const body = `
   <!doctype html>
   <title>Service worker HTML generation</title>
   <h1>
   The URL is ${event.request.url}
   </h1>
+  <ul>
+    <li>Cache: ${event.request.cache}</li>
+    <li>Credentials: ${event.request.credential}</li>
+    <li>Destination: ${event.request.destination}</li>
+    <li>Method: ${event.request.method}</li>
+    <li>Referrer: ${event.request.referrer}</li>
+  </ul>
   `
 
   // the response constructure takes a few parameters, the first one is body, second status, then status text, and then headers
